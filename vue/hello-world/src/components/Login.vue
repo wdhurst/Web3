d2beb0e9c1a18af2e5529efcc68cdc1ad8e43155
Wyatt Hurst <wdhurst@mavs.coloradomesa.edu>
2018-09-07 14:35:01 -0600

    working

---
+++ vue/hello-world/src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <form>
+        Username: <input type="text" name="username"/>
+        Password: <input type="text" name="password"/>
+    </form>
+</template>
+
+<script> 
+export default {
+    name: 'Login',
+    props: {
+    }
+}
+</script>
